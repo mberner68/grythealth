204ddac33eafb86c9f313e6a4c7b0d5aed24587a
mberner68 <mbe_68@yahoo.com>
2021-11-04 19:58:51 -0400

    Add files via upload

---
+++ GrytBulletinBoard/CoreLibrary/Models/NoteModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+
+namespace CoreLibrary.Models
+{
+    public class NoteModel
+    {
+        public int Id { get; set; }
+        public string Board { get; set; }
+        public Guid NodeKey { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public string ClassName { get; set; }
+        public string Creator { get; set; }
+        public string BoardKey { get; set; }
+        public int PageId { get; set; }
+        public string NewBoardKey { get; set; }
+        public string question1 {get; set;}
+      //  public string Avatar { get; set; }
+      //  public HttpPostedFileBase File { get; set; }
+        public NoteModel() { }
+        
+    }
+}
---
+++ GrytBulletinBoard/CoreLibrary/Models/NoteRecord.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CoreLibrary.Models
+{
+    public class NoteRecord
+    {
+        public string Title { get; set; }
+        public string ClassName { get; set; }
+        public string Content { get; set; }
+        public string Key { get; set; }
+        public string Creator { get; set; }
+    }
+}
